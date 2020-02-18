7a0d84e0589bcec095f457db0cc27373a9015874
lunakoly <lunyak.kolya@mail.ru>
2020-02-18 23:45:43 +0300

    initial

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const express = require('express')
-
 const app = express()
+
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+
+
+
+io.on('connection', function(socket) {
+    console.log('User connected')
+})
 
 
 app.get('/', function(request, response) {
@@ -9,6 +18,6 @@ app.get('/', function(request, response) {
 
 app.use(express.static('static'))
 
-app.listen(1234, function() {
+http.listen(1234, function() {
     console.log('Server started on port 1234')
 })
